a73d51db5c7013cf03481288b555abd566349a6a
Christian <mail@christianhohenbild.de>
2024-03-10 13:37:16 +0100

    remove box

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,6 @@ import './index.css'
 import { Particles } from './Particles.jsx'
 
 
-function Box(props) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef()
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
-  // Return the view, these are regular Threejs elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => (event.stopPropagation(), hover(true))}
-      onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
-
-
 function App() {
   
   return (
@@ -41,7 +17,6 @@ function App() {
       fov: 40 }}  
     >
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <Box position={[0, 0, 0]} />
       <Particles position = {[1.5, 0, 0]} />
       <OrbitControls />
     </Canvas>
